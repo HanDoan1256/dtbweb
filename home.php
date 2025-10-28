8afcb5ed32685d355614dc6f1e647fae663e9b52
qunhhnhw <quynhnhungo06@gmail.com>
2025-10-29 00:03:41 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,70 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" href="/styles/style.css" />
+</head>
+
+<body>
+  <?php include 'header.inc'; ?>
+
+  <main>
+    <section class="intro">
+      <div class="image">
+        <img src="/archer.jpg" alt="Become an all-star">
+      </div>
+    </section>
+
+    <section class="top-archer">
+      <div class="lb-head">
+        <h2 id="lb-title">TOP ARCHER THIS WEEK</h2>
+        <div class="lb-range">(dd/mm – dd/mm)</div>
+      </div>
+
+      <div class="table-content">
+        <div class="table-left">
+          <div class="table-entry">
+            <h3 class="table-name">NAME</h3>
+            <p class="table-meta">Club</p>
+            <p class="table-meta">Highest score</p>
+          </div>
+
+          <div class="table-entry">
+            <h3 class="table-name">NAME</h3>
+            <p class="table-meta">Club</p>
+            <p class="table-meta">Highest score</p>
+          </div>
+
+          <div class="table-entry">
+            <h3 class="table-name">NAME</h3>
+            <p class="table-meta">Club</p>
+            <p class="table-meta">Highest score</p>
+          </div>
+        </div>
+
+        <div class="table-right">
+          <div class="table-entry">
+            <h3 class="table-name">NAME</h3>
+            <p class="table-meta">Club</p>
+            <p class="table-meta">Highest score</p>
+          </div>
+
+          <div class="table-entry">
+            <h3 class="table-name">NAME</h3>
+            <p class="table-meta">Club</p>
+            <p class="table-meta">Highest score</p>
+          </div>
+
+          <div class="table-entry">
+            <h3 class="table-name">NAME</h3>
+            <p class="table-meta">Club</p>
+            <p class="table-meta">Highest score</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <?php include 'footer.inc'; ?>
+</body>
+</html>
